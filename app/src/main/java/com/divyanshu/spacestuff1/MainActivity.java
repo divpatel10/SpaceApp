6ca2f59f462af08ae9c3d2fc2d74966a5d954782
div <div.pat499@gmail.com>
2021-01-30 10:45:13 -0500

    Initial Commit

---
+++ app/src/main/java/com/divyanshu/spacestuff1/MainActivity.java
@@ -0,0 +1,16 @@
+package com.divyanshu.spacestuff1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //First Commit
+
+    }
+}
