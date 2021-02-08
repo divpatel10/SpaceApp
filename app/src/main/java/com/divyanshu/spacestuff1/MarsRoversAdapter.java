fe12258c36dd9f647976ba31c4d4bfb38d45cf1b
div <div.pat499@gmail.com>
2021-02-07 22:51:06 -0500

    Added ImageButton over the Recyclerview item layout and implemented download functionality for the rover images.

--- app/src/main/java/com/divyanshu/spacestuff1/MarsRoversAdapter.java
+++ app/src/main/java/com/divyanshu/spacestuff1/MarsRoversAdapter.java
@@ -1,12 +1,17 @@
 package com.divyanshu.spacestuff1;
 
+import android.app.DownloadManager;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -17,8 +22,9 @@ import java.util.ArrayList;
 
 public class MarsRoversAdapter extends RecyclerView.Adapter<MarsRoversAdapter.MarsRoversViewHolder> {
 
-    private Context mContext;
-    private ArrayList<RoverModel>  roverModelArrayList;
+    private final Context mContext;
+    private final ArrayList<RoverModel>  roverModelArrayList;
+
 
     public MarsRoversAdapter(Context context, ArrayList<RoverModel> roverModels){
         mContext = context;
@@ -45,12 +51,36 @@ public class MarsRoversAdapter extends RecyclerView.Adapter<MarsRoversAdapter.Ma
 
         Picasso.get().load(imageURL).fit().centerInside().placeholder(R.drawable.icon_update).into(holder.imageView);
 
+
+
             holder.itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
                     Intent intent = new Intent(mContext.getApplicationContext(), FullScreenImage.class);
                     intent.putExtra("image", imageURL);
                     mContext.startActivity(intent);
+                }
+            });
+
+            holder.imageButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    try {
+
+                        Uri uri = Uri.parse(imageURL);
+                        DownloadManager downloadManager = (DownloadManager) mContext.getSystemService(Context.DOWNLOAD_SERVICE);
+                        DownloadManager.Request request = new DownloadManager.Request(uri);
+                        request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI | DownloadManager.Request.NETWORK_MOBILE);
+                        String fileName = imageURL.substring(imageURL.lastIndexOf('/') + 1);
+                        request.setTitle(fileName);
+                        request.allowScanningByMediaScanner();
+                        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+                        request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS,"/images");
+                        request.setMimeType("*/*");
+                        downloadManager.enqueue(request);
+                    } catch (Exception e){
+                        Toast.makeText(mContext.getApplicationContext(), "Error Downloading", Toast.LENGTH_SHORT).show();
+                    }
                 }
             });
     }
@@ -64,11 +94,13 @@ public class MarsRoversAdapter extends RecyclerView.Adapter<MarsRoversAdapter.Ma
     public class MarsRoversViewHolder extends RecyclerView.ViewHolder{
 
         public ImageView imageView;
-
+        public ImageButton imageButton;
         public MarsRoversViewHolder(@NonNull View itemView) {
             super(itemView);
             imageView = itemView.findViewById(R.id.mars_rover_image);
-
+            imageButton = itemView.findViewById(R.id.rover_image_download);
         }
+
+
     }
 }
